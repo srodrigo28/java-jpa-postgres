e72d5d952b4f47f18fd80d13a296d85d4b170f96
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-12-29 08:02:47 -0300

    TB_PUBLISHER criado com sucesso

---
+++ src/main/java/com/treinamento/dev/model/PublisherModel.java
@@ -0,0 +1,41 @@
+package com.treinamento.dev.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PUBLISHER")
+public class PublisherModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
